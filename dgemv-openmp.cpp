aeb48b0757285850cabfb29ccf44387817f1bd91
ryklin2 <dmoguy@gmail.com>
2025-11-16 21:47:33 -0800

    Update dgemv-openmp.cpp

---
+++ dgemv-openmp.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h> 
+void dgemv(int M, int N, double* A, double* x, double* y) {
+    
+
+    #pragma omp parallel for
+    for (int i = 0; i < M; i++) {
+        
+
+        double sum = 0.0;
+        
+
+        for (int j = 0; j < N; j++) {
+
+            sum += A[i + j*M] * x[j];
+        }
+
+        y[i] += sum;
+    }
+}
+
+int main() {
+    const int M = 128;
+    const int N = 64;
+
+    double* A = (double*) malloc(M * N * sizeof(double));
+    double* x = (double*) malloc(N * sizeof(double));
+    double* y = (double*) malloc(M * sizeof(double));
+
+    if (!A || !x || !y) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    dgemv(M, N, A, x, y);
+
+    // Clean up
+    free(A);
+    free(x);
+    free(y);
+
+    return 0;
+}
+
+
